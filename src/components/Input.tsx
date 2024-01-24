9f40f318bed1cdd8420dd845c753f8d4f0706bb1
Renato <renatomarquesteles@gmail.com>
2024-01-24 14:11:58 -0300

    chore: add input component and tamagui size tokens

---
+++ src/components/Input.tsx
@@ -0,0 +1,22 @@
+import { Input as TamaguiInput, InputProps } from 'tamagui';
+
+export function Input({ ...rest }: InputProps) {
+  return (
+    <TamaguiInput
+      w="$full"
+      h="$14"
+      px="$4"
+      borderWidth={0}
+      fontSize="$md"
+      bc="$gray700"
+      color="$white"
+      mb="$4"
+      placeholderTextColor="$gray300"
+      focusStyle={{
+        borderWidth: 1,
+        borderColor: '$green500',
+      }}
+      {...rest}
+    />
+  );
+}
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,12 +2,13 @@ import { Heading, Image, Text, View, YStack } from 'tamagui';
 
 import backgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
+import { Input } from '@components/Input';
 
 export function SignIn() {
   return (
-    <YStack f={1} bg="$gray700">
+    <YStack f={1} bg="$gray700" px="$10">
       <Image source={backgroundImg} resizeMode="contain" position="absolute" />
-      <View ai="center" my={94}>
+      <View ai="center" my="$24">
         <LogoSvg />
         <Text color="$gray100" fontSize="$sm">
           Train your body and your mind.
@@ -15,9 +16,16 @@ export function SignIn() {
       </View>
 
       <View ai="center">
-        <Heading color="$gray100" fontSize="$xl" mb={100}>
+        <Heading color="$gray100" fontSize="$xl" mb="$6">
           Access your account
         </Heading>
+
+        <Input
+          placeholder="Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Password" secureTextEntry />
       </View>
     </YStack>
   );
